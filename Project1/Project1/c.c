1300f5eb89b4c33678742cfa64cf755deeb6cd62
engineer-QIU <2311674683@qq.com>
2021-08-31 09:38:29 +0800

    提交代码

---
+++ Project1/Project1/c.c
@@ -0,0 +1,18 @@
+//#include<stdio.h>
+//int main()
+//{
+//	printf("hello world\n");
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+	return 0;
+}
