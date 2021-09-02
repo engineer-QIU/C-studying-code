dc3a2abae6200aa418cd306bbfd292fb9126bfb2
engineer-QIU <2311674683@qq.com>
2021-09-02 10:55:34 +0800

    提交代码

---
+++ Project3/Project3/源.cpp
@@ -0,0 +1,52 @@
+//#include<stdio.h>
+//#include<string.h>
+//
+// int Max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	int max = 0;
+//	//scanf("%d%d", &num1,&num2);
+//	max = Max(num1, num2);
+//	printf("%d\n", max);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[] = { 0,1,2,3,4,5,6 };
+//	printf("%d\n", sizeof(arr)/sizeof(arr[0]));
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = ~a;
+	printf("%d\n", b);
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
